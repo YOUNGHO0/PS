043acfbf5a1d2f7ae8ee50fbadf657a072ac8690
YOUNGHO0 <31pqopq13@gmail.com>
2024-11-24 00:13:34 +0900

    Update 최소 스패닝 트리.cc
    작성한 정답으로변경

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.cc
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.cc
@@ -1,80 +1,90 @@
 #include <bits/stdc++.h>
+#include <iostream>
+
 using namespace std;
 
-int parent[10002];  // 부모 배열
+int parent[10002] = {0};
 
-// Find with path compression
-int find(int a) {
-    if (parent[a] != a) {
-        parent[a] = find(parent[a]);  // 경로 압축
+int find( int a){
+    if(parent[a] !=a){
+        parent[a] = find(parent[a]);
     }
     return parent[a];
 }
 
-// Union without rank
-void unionNumber(int a, int b) {
+
+void unionNumber(int a, int b){
+
     int pa = find(a);
     int pb = find(b);
-
-    if (pa != pb) {
-        parent[pb] = pa;  // 두 집합을 합침
-    }
+    parent[pb] = pa;
 }
 
-class Edge {
+
+
+class Edge{
+
 public:
-    int from, to;
-    long value;
 
-    Edge(int from, int to, long value) : from(from), to(to), value(value) {}
-};
+    Edge(int from,int to, long value):from(from),to(to),value(value){
 
-// 우선순위 큐 비교 함수
-struct CompareEdge {
-    bool operator()(const Edge& a, const Edge& b) {
-        return a.value > b.value;  // 가중치가 작은 간선이 우선으로 오도록 설정
     }
+
+    int from;
+    int to;
+    long value;
+
+
 };
 
-int main() {
-    int nodeNum, edgeNum;
-    cin >> nodeNum >> edgeNum;
+int main(){
 
-    priority_queue<Edge, vector<Edge>, CompareEdge> pq;  // 우선순위 큐 (Min-Heap)
+    int nodeNum;
+    int edgeNum;
+    cin >> nodeNum;
+    cin >> edgeNum;
 
-    int from, to;
+    auto cmp = [](Edge a, Edge b){
+
+        return a.value > b.value;
+    };
+    priority_queue<Edge,vector<Edge>,decltype(cmp)> pq(cmp);
+
+    int from;
+    int to;
     long value;
-    for (int i = 0; i < edgeNum; i++) {
+    for(int i =0; i<edgeNum; i++){
         cin >> from >> to >> value;
-        pq.push(Edge(from, to, value));  // 간선을 우선순위 큐에 삽입
+        Edge e(from,to,value);
+        pq.push(e);
     }
 
-    // 부모 배열 초기화: 각 정점은 자기 자신을 부모로 설정
-    for (int i = 1; i <= nodeNum; i++) {
+    
+    for(int i =0; i<10002; i++){
         parent[i] = i;
     }
 
-    long answer = 0;
+    long answer=0;
 
-    // Kruskal 알고리즘: 간선들을 하나씩 처리
-    int edgeCount = 0;
-    while (!pq.empty()) {
-        Edge e = pq.top();
+    while(!pq.empty()){
+        Edge temp = pq.top();
         pq.pop();
 
-        if (find(e.from) != find(e.to)) {  // 두 정점이 서로 연결되어 있지 않으면
-            unionNumber(e.from, e.to);  // 두 집합을 합침
-            answer += e.value;  // 해당 간선의 가중치를 MST에 추가
-            edgeCount++;
-
-            // MST는 V-1개의 간선으로 구성됨
-            if (edgeCount == nodeNum - 1) {
-                break;
-            }
+        if(find(parent[temp.from]) != find(parent[temp.to])){
+            unionNumber(temp.from,temp.to);
+            answer+=temp.value;
         }
+        
     }
 
-    cout << answer << endl;  // 최소 스패닝 트리의 가중치 합 출력
+    cout << answer << endl;
+
+    
+  
+
+
+
+
 
     return 0;
 }
