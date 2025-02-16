a11c889d90544d65250b4a44a3f56989d653e8dd
YOUNGHO0 <31pqopq13@gmail.com>
2025-02-16 14:40:12 +0900

    [Platinum IV] Title: 노트북의 주인을 찾아서, Time: 0 ms, Memory: 2104 KB -BaekjoonHub

--- 백준/Platinum/1298. 노트북의 주인을 찾아서/노트북의 주인을 찾아서.cc
+++ 백준/Platinum/1298. 노트북의 주인을 찾아서/노트북의 주인을 찾아서.cc
@@ -2,44 +2,61 @@
 
 using namespace std;
 
-map<int, vector<int>> noteBookList;
+map<int,vector<int>> noteBookList;
+
 int noteBookVisited[10004] = {0};
 int visited[10004] = {0};
+bool canUse(int personNumber){
 
-bool canUse(int personNumber) {
-    for (int noteBookNumber : noteBookList[personNumber]) {
 
-        if(visited[noteBookNumber] !=0) continue;
+    vector<int> noteList = noteBookList[personNumber];
 
-        visited[noteBookNumber] = 1;
+    for(auto noteBookNumber : noteList){
 
-        if(noteBookVisited[noteBookNumber] == 0 || canUse(noteBookVisited[noteBookNumber])){
+        if(visited[noteBookNumber] !=0 )continue;
+        visited[noteBookNumber] =1;
+
+
+        if(noteBookVisited[noteBookNumber] ==0){
             noteBookVisited[noteBookNumber] = personNumber;
-            return true;
+            return true; 
+        }
+        else if(noteBookVisited[noteBookNumber] != 0){
+            int alreadyPersoNumber = noteBookVisited[noteBookNumber];
+            if( canUse(alreadyPersoNumber)){
+                noteBookVisited[noteBookNumber] = personNumber;
+                return true; 
+            }
+
         }
 
     }
     return false;
+
 }
 
-int main() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
+
+
+
+int main(){
+
     int totalNumber, people;
+
     cin >> totalNumber >> people;
 
-    for (int i = 0; i < people; i++) {
+    for(int i =0; i<people; i++){
+
         int from, to;
         cin >> from >> to;
         noteBookList[from].push_back(to);
     }
-
     int ans = 0;
-    for (int i = 1; i <= totalNumber; i++) {
-       fill(visited, visited + totalNumber + 1, 0);  // 매칭 시도할 때마다 방문 배열 초기화
-        if (canUse(i)) ans++;
+    for(int i =1; i<=totalNumber; i++){
+        memset(visited, 0 ,sizeof(visited));
+        if(canUse(i)) ans++;
     }
 
     cout << ans << endl;
     return 0;
+
 }
